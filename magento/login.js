8d79ac2596ee24d124e0c3afe467e044e8921c01
andriraymond <andriraymondo@gmail.com>
2024-06-21 07:54:49 +0700

    feat(#2) : Add Magento checkout-product test cases)

--- magento/login.js
+++ magento/login.js
@@ -1,26 +1,34 @@
 const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
- 
-;(async function magento() {
-  let driver = await new Builder().forBrowser(Browser.FIREFOX).build()
-  try {
-    await driver.get('https://magento.softwaretestingboard.com/customer/account/login')
+const assert = require("assert");
 
-    await driver.findElement(By.id('email')).sendKeys('blaisepascal@yopmail.com')
-    await driver.findElement(By.id('pass')).sendKeys('Qatest123456')
-    await driver.findElement(By.id('send2')).click()
-    
+async function loginTest(email, password, expectedTitle) {
+  let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+  try {
+    await driver.get(
+      "https://magento.softwaretestingboard.com/customer/account/login"
+    );
+
+    await driver.findElement(By.id("email")).sendKeys(email);
+    await driver.findElement(By.id("pass")).sendKeys(password);
+    await driver.findElement(By.id("send2")).click();
+
     // Wait for the title to be 'My Account'
-    await driver.wait(until.titleIs('My Account'), 1000);
+    await driver.wait(until.titleIs(expectedTitle), 5000);
 
     // Verify the title
     let title = await driver.getTitle();
-    assert.strictEqual(title, 'My Account');
-
-    console.log('Login test passed successfully.');
+    assert.strictEqual(title, expectedTitle);
+    console.log(`passed - login test with email '${email}' and password '${password}' passed successfully.`);
 
   } catch (error) {
-    console.error('Login test failed:', error);
+    console.log(
+      `failed - login test with email '${email}' and password '${password}' failed`);
   } finally {
-    await driver.quit()
+    await driver.quit();
   }
-})()
+}
+
+// Define different test cases with various credentials and expected results
+loginTest("blaisepascal@yopmail.com", "Qatest1234", "My Account");
+loginTest("blaise@yopmail.com", "Qatest123456", "My Account");
+loginTest("blaisepascal@yopmail.com", "Qatest123456", "My Account");
